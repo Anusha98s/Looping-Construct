483be8837c6df28ece28c3c0c4e57b12ac736331
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 10:27:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Looping_Construct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Your Name\n");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Email...\n");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter Your MobileNo...\n");
+            int MobileNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your name is" + name + " Your Email is" + email + " Your MobileNo is" + MobileNo);
+        }
+    }
+}
